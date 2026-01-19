18c99193e5b0214ebde35cd71d737ff1875214b3
HeoSeonJin0504 <sonjin54@naver.com>
2026-01-19 20:28:38 +0900

    프로젝트 구조 설정 및 의존성 추가

--- src/main/java/com/example/aienglishtrainer/controller/TestController.java
+++ src/main/java/com/example/aienglishtrainer/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.example.aienglishtrainer;
+package com.example.aienglishtrainer.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
